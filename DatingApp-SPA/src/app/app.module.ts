6555780cb08ac0a4ee06d2d0b1594e9a8582e86a
ErikMóczi <32465761+ErikMoczi@users.noreply.github.com>
2019-06-10 11:47:27 +0200

    End of Section 5

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule} from '@angular/forms';
 import {AuthService} from './_services/auth.service';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
+import {ErrorInterceptorProvider} from './_services/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,10 @@ import {RegisterComponent} from './register/register.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
